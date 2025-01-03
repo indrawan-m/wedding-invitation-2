bfd455e3e3f8b22c475034cffd812d1f8b474be7
indrawan-m <mohamadindrawan2@gmail.com>
2025-01-03 12:57:10 +0700

    wedding invitation 1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sacramento: ['Sacramento', 'cursive'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
